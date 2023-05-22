f1f7614ccd20fd9dfa7ab0208f0134f2318aee07
MathieuLebrun <113026852+MathieuLebrun@users.noreply.github.com>
2023-05-22 16:59:54 +0200

    Update login_request.dart

--- mobile_mds/lib/models/login_request.dart
+++ mobile_mds/lib/models/login_request.dart
@@ -9,6 +9,10 @@ class LoginRequestModel {
   late final String action;
   late final String login;
   late final String password;
+  late final String token;
+
+  LoginRequestModel.fromJson(Map<String, dynamic> json) {
+    action =  json['action'];
     login = json['login'];
     password = json['password'];
     token = json['token'];

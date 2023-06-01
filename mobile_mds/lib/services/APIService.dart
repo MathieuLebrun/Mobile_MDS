63b1a97da9b18a1d2736cb06d03bd6fcb2bbb3fb
MathieuLebrun <113026852+MathieuLebrun@users.noreply.github.com>
2023-06-01 21:48:34 +0200

    Update APIService.dart

--- mobile_mds/lib/services/APIService.dart
+++ mobile_mds/lib/services/APIService.dart
@@ -1,13 +1,13 @@
-
-import 'dart:convert';
-
 import 'package:http/http.dart' as http;
+import 'package:mobile_mds/models/tachedev_request.dart';
 
 import '../models/login_request.dart';
 class APIService {
   static var client = http.Client();
 
-  static Future<http.Response> login(
+
+  /* login */
+  static Future<http.Response> postdata(
     LoginRequestModel model,params
   ) async {
     Map<String, String> requestHeaders = {
@@ -18,18 +18,55 @@ class APIService {
     var url = "http://217.13.54.98/operationnel/Internal/API/index.cfm";
 
     String queryString = Uri(queryParameters: params).query;
-  String requestUrl = '$url?$queryString';
+    String requestUrl = '$url?$queryString';
 
-  // Effectuer la requête HTTP
-  http.Response response = await http.post(Uri.parse(requestUrl));
-  return response; 
-/*
-    http.Response response = await client.post(
-      Uri.parse(url),
-      headers: requestHeaders,
-      body:// jsonEncode(model.toJson()),
-      jsonEncode(params),
-    );
-    return response; */
+    // Effectuer la requête HTTP
+    http.Response response = await http.post(Uri.parse(requestUrl));
+    return response; 
+
   } 
+
+  /* Top 5 datatable | Récupérer les projets, les sous projets et les clients*/
+  static Future<http.Response> getdata(
+    params
+  ) async {
+    Map<String, String> requestHeaders = {
+      'Content-Type': 'application/json', // Ajoutez les en-têtes spécifiques requis par votre API
+      'Authorization': 'Bearer your_token_here',
+    };
+
+    var url = "http://217.13.54.98/operationnel/Internal/API/index.cfm";
+
+    String queryString = Uri(queryParameters: params).query;
+    String requestUrl = '$url?$queryString';
+
+    // Effectuer la requête HTTP
+    http.Response response = await http.get(Uri.parse(requestUrl));
+    return response; 
+
+  }
+
+  /* tache */
+  static Future<http.Response> posttache(
+    TacheRequestModel model,params
+  ) async {
+    Map<String, String> requestHeaders = {
+      'Content-Type': 'application/json', // Ajoutez les en-têtes spécifiques requis par votre API
+      'Authorization': 'Bearer your_token_here',
+    };
+
+    var url = "http://217.13.54.98/operationnel/Internal/API/index.cfm";
+
+    String queryString = Uri(queryParameters: params).query;
+    String requestUrl = '$url?$queryString';
+
+    // Effectuer la requête HTTP
+    http.Response response = await http.post(Uri.parse(requestUrl));
+    return response; 
+
+  } 
+
+  static tacheResponseJson(TacheRequestModel model, Map<String, String> queryParams) {} 
 }
+
+  

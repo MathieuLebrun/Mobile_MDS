467f262fd0aac9f2aa35d8e98ec081d8090cff96
MathieuLebrun <113026852+MathieuLebrun@users.noreply.github.com>
2023-06-01 21:48:30 +0200

    Create tachedev_request.dart

---
+++ mobile_mds/lib/models/tachedev_request.dart
@@ -0,0 +1,57 @@
+class TacheRequestModel {
+  TacheRequestModel({
+    required this.action,
+    required this.token,
+    required this.semaine,
+    required this.annee,
+    required this.type,
+    required this.projet,
+    required this.sousProjet,
+    required this.client,
+    required this.tache,
+    required this.commentaire,
+    required this.duree,
+  });
+
+  late final String action;
+  late final String token;
+  late final int semaine;
+  late final int annee;
+  late final String type;
+  late final String projet;
+  late final String sousProjet;
+  late final int client;
+  late final String tache;
+  late final String commentaire;
+  late final double duree;
+
+  TacheRequestModel.fromJson(Map<String, dynamic> json) {
+    action =  json['action'];
+    token = json['token'];
+    semaine = json['semaine'];
+    annee = json['annee'];
+    type =  json['type'];
+    projet = json['projet'];
+    sousProjet = json['sousProjet'];
+    client = json['client'];
+    tache =  json['tache'];
+    commentaire = json['commentaire'];
+    duree = json['duree'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['action'] =action;
+    data['token'] = token;
+    data['semaine'] = semaine;
+    data['annee'] = annee;
+    data['type'] = type;
+    data['projet'] = projet;
+    data['sousProjet'] = sousProjet;
+    data['client'] = client;
+    data['tache'] = tache;
+    data['commentaire'] = commentaire;
+    data['duree'] = duree;
+    return data;
+  }
+}
